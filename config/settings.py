55ae015df3a1ed3ef292c842e597ca59bcb7205c
Herriot DAGOUDI <herriotdagoudi@gmail.com>
2026-06-11 11:10:50 +0100

    Move Django secret key to environment

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&p@z&9$&-7fc^+m%s5%t9%&j9$0*%ms8nnysl-h9rza_eb%ihh'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-dev-only-change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
